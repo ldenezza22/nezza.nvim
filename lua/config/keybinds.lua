6c42b2c2ac70fab2ad271704dd7324ba27313278
Logan DeNezza <logan.denezza@proton.me>
2026-03-17 22:24:44 -0400

    Added keybinds to go to definition/reference\nAllowed docstring popups

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -2,9 +2,18 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
 
 vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover info" })
+vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, { desc = "Signature help" })
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Line diagnostics" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Diagnostics list" })
-
-vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action) 
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
 vim.keymap.set("n", "gd", vim.lsp.buf.definition)
 vim.keymap.set("n", "gr", vim.lsp.buf.references)
+vim.keymap.set("n", "<leader>fc", function()
+	vim.lsp.buf.format()
+end, { desc = "Format file" })
+vim.api.nvim_create_autocmd("InsertCharPre", {
+	callback = function()
+		vim.lsp.buf.signature_help()
+	end,
+})
