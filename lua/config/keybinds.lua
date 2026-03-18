d8a3c3ac64f01dee65066c51ad8c8fefec3ab8bf
Logan DeNezza <logan.denezza@proton.me>
2026-03-17 22:00:29 -0400

    Added ruff for linting for python

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,2 +1,10 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
+
+vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover info" })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Line diagnostics" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Diagnostics list" })
+
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action) 
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "gr", vim.lsp.buf.references)
